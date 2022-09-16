ca776eb26ce00c37a43a41bd0517d8408b63badc
mrivero40 <matiasrivero40@hotmail.com>
2022-09-16 19:22:57 -0300

    maquetación de html, función tendencias de movies, url's de API + API-KEY

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-console.log('funciono');
+const URL_TRENDING = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
+const URL_IMG = 'https://image.tmdb.org/t/p/w300';
+
+async function getTrendingMoviesPreview() {
+    const res = await fetch(URL_TRENDING);
+    const data = await res.json();
+    const movies = data.results;
+    console.log(movies);
+
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', `${URL_IMG}${movie.poster_path}`);
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+};
+getTrendingMoviesPreview();
